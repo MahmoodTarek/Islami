9293e58c08a5da76008e7368132b7702cfca855a
DELL <dev.mahmoudtarek@gmail.com>
2026-02-24 23:22:53 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/mTm/islami/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mTm.islami
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:islami/islami_app.dart';
+
+void main() {
+  runApp(IslamiApp());
+}
