d4740f65f0fffb3e8f3aed7dfdae427312e897da
DELL <dev.mahmoudtarek@gmail.com>
2026-03-01 04:06:12 +0200

    implement onboarding screen with multi-page introduction and shared header component

---
+++ lib/utils/widgets/islami_header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:islami/utils/app_assets.dart';
+import 'package:islami/utils/app_colors.dart';
+import 'package:islami/utils/app_strings.dart';
+
+class IslamiHeader extends StatelessWidget {
+  const IslamiHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textGradient = LinearGradient(
+      colors: [AppColors.camel, AppColors.softYellow],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.only(top: 75, right: 65, left: 65, bottom: 20),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppAssets.bgMosque),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final shader = textGradient.createShader(
+            Rect.fromLTWH(0, 50, constraints.maxWidth, 250),
+          );
+          return Text(
+            AppStrings.appName,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 80,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Kamail',
+              foreground: Paint()..shader = shader,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
