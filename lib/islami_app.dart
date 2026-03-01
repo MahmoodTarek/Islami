09f947b2d58651830dd86a6d10c14b723bbe046e
DELL <dev.mahmoudtarek@gmail.com>
2026-03-01 04:07:17 +0200

    optimize islami_app.dart by adding a const constructor and removing unused imports

--- lib/islami_app.dart
+++ lib/islami_app.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:islami/ui/home/home.dart';
 import 'package:islami/ui/on_boarding/on_boarding_pages.dart';
 import 'package:islami/utils/app_colors.dart';
-import 'package:islami/utils/app_launch_service.dart';
 import 'package:islami/utils/app_routes.dart';
 import 'package:islami/utils/app_theme.dart';
-import 'package:shared_preferences/src/shared_preferences_legacy.dart';
 
 class IslamiApp extends StatelessWidget {
   final bool isFirstLaunch;
 
-  IslamiApp({super.key, required this.isFirstLaunch});
+  const IslamiApp({super.key, required this.isFirstLaunch});
 
   @override
   Widget build(BuildContext context) {
