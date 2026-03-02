d1157f520a0523ba041b9283a5a4df23169c72c7
DELL <dev.mahmoudtarek@gmail.com>
2026-03-02 20:14:20 +0200

    implement SurahSearchField widget for home screen search functionality

---
+++ lib/ui/home/widgets/islami_search_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:islami/utils/app_assets.dart';
+import 'package:islami/utils/app_colors.dart';
+import 'package:islami/utils/app_strings.dart';
+
+import '../../../utils/app_styles.dart';
+
+class SurahSearchField extends StatelessWidget {
+  const SurahSearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final OutlineInputBorder baseBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15),
+      borderSide: BorderSide(color: AppColors.gold),
+    );
+
+    return TextField(
+      decoration: InputDecoration(
+        border: baseBorder,
+        enabledBorder: baseBorder,
+        focusedBorder: baseBorder,
+        filled: true,
+        fillColor: AppColors.darkGray.withValues(alpha: 0.7),
+        hintText: AppStrings.homeSearchHint,
+        hintStyle: AppStyles.base16BoldCreamyYellow,
+        prefixIcon: Padding(
+          padding: const EdgeInsets.all(14),
+          child: SvgPicture.asset(AppIcons.icSearch),
+        ),
+        contentPadding: const EdgeInsets.all(14),
+      ),
+      onTapOutside: (PointerDownEvent event) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      style: AppStyles.base16BoldCreamyYellow,
+      cursorColor: AppColors.gold,
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+    );
+  }
+}
