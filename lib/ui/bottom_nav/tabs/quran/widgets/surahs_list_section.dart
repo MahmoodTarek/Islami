6655738ad7c355e76feb25aa36c2e2a7a8e202b2
DELL <dev.mahmoudtarek@gmail.com>
2026-03-03 06:55:05 +0200

    feat: Add new assets and UI for Quran tab

---
+++ lib/ui/bottom_nav/tabs/quran/widgets/surahs_list_section.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:islami/models/surah.dart';
+import 'package:islami/ui/bottom_nav/tabs/quran/widgets/surah_card.dart';
+import 'package:islami/utils/app_colors.dart';
+
+class SurahsSection extends StatelessWidget {
+  const SurahsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.separated(
+      itemCount: allSurahs.length,
+      itemBuilder: (context, index) => QuranSurahCard(surah: allSurahs[index]),
+      separatorBuilder: (_, _) => Divider(
+        height: 20,
+        thickness: 1.5,
+        color: AppColors.white,
+        endIndent: 64,
+        indent: 64,
+      ),
+    );
+  }
+}
