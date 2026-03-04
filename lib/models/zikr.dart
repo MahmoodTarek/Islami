cd21deb6441d4082f8816b3f79d51519d7e32e86
DELL <dev.mahmoudtarek@gmail.com>
2026-03-04 08:39:37 +0200

    feat: Add Sebha widget for Azkar tab

---
+++ lib/models/zikr.dart
@@ -0,0 +1,6 @@
+class Zikr {
+  final String name;
+  final String aya;
+
+  const Zikr({required this.name, required this.aya});
+}
--- lib/ui/islami_bottom_nav/tabs/azkar/azkar.dart
+++ lib/ui/islami_bottom_nav/tabs/azkar/azkar.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:islami/models/zikr.dart';
+import 'package:islami/ui/islami_bottom_nav/tabs/azkar/widgets/sebha.dart';
+import 'package:islami/ui/islami_bottom_nav/widgets/islami_page_layout.dart';
+import 'package:islami/utils/app_strings.dart';
 
-class AzkarTab extends StatelessWidget {
+import '../../../../utils/app_assets.dart';
+import '../../../../utils/app_styles.dart';
+
+class AzkarTab extends StatefulWidget {
   const AzkarTab({super.key});
 
   @override
+  State<AzkarTab> createState() => _AzkarTabState();
+}
+
+class _AzkarTabState extends State<AzkarTab> {
+  List<Zikr> azkarList = [
+    Zikr(
+      name: AppStrings.tabAzkarSubhanAllah,
+      aya: AppStrings.tabAzkarSubhanAllahAya,
+    ),
+    Zikr(
+      name: AppStrings.tabAzkarAlhamdulillah,
+      aya: AppStrings.tabAzkarAlhamdulillahAya,
+    ),
+    Zikr(
+      name: AppStrings.tabAzkarAllahAkbar,
+      aya: AppStrings.tabAzkarAllahAkbarAya,
+    ),
+  ];
+  int currentZikrIndex = 0;
+  int currentCount = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(child: Text('Azkar',style: TextStyle(fontSize: 24,color: Colors.white),)),
-      ],
+    return IslamiPageLayout(
+      scrollable: true,
+      backgroundImagePath: AppImages.bgAzkar,
+      content: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 24),
+        child: Column(
+          spacing: 16,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              azkarList[currentZikrIndex].aya,
+              style: AppStyles.bold20White.copyWith(fontSize: 36),
+              textAlign: TextAlign.center,
+            ),
+            Sebha(
+              currentZikr: azkarList[currentZikrIndex].name,
+              count: currentCount,
+              onTap: () {
+                addOnZikr();
+              },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void addOnZikr() {
+    setState(() {
+      if (currentCount == 33) {
+        currentCount = 0;
+        currentZikrIndex = (currentZikrIndex + 1) % azkarList.length;
+      } else {
+        currentCount++;
+      }
+    });
   }
 }
