914900b1f37ea4fd13a522d5fba6de7adf96da5f
DELL <dev.mahmoudtarek@gmail.com>
2026-03-05 09:09:30 +0200

    feat: Implement cycle completion state and UI for Azkar tab
    - Introduce `isCycleCompleted` state to manage the visibility of the Azkar content.
    - Add `AzkarCompletedView` widget to be displayed when the user finishes all azkar in the list.
    - Rename `currentCount` to `currentZikrCount` and `addOnZikr` to `plusZikr`.
    - Update logic to reset the cycle and navigate back to the first zikr upon completion.

--- lib/ui/islami_bottom_nav/tabs/azkar/azkar.dart
+++ lib/ui/islami_bottom_nav/tabs/azkar/azkar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islami/models/zikr.dart';
+import 'package:islami/ui/islami_bottom_nav/tabs/azkar/widgets/azkar_completed_view.dart';
 import 'package:islami/ui/islami_bottom_nav/tabs/azkar/widgets/sebha.dart';
 import 'package:islami/ui/islami_bottom_nav/widgets/islami_page_layout.dart';
 import 'package:islami/utils/app_strings.dart';
@@ -30,7 +31,8 @@ class _AzkarTabState extends State<AzkarTab> {
     ),
   ];
   int currentZikrIndex = 0;
-  int currentCount = 0;
+  int currentZikrCount = 0;
+  bool isCycleCompleted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,36 +40,49 @@ class _AzkarTabState extends State<AzkarTab> {
       scrollable: true,
       backgroundImagePath: AppImages.bgAzkar,
       content: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 24),
-        child: Column(
-          spacing: 16,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              azkarList[currentZikrIndex].aya,
-              style: AppStyles.bold20White.copyWith(fontSize: 36),
-              textAlign: TextAlign.center,
-            ),
-            Sebha(
-              currentZikr: azkarList[currentZikrIndex].name,
-              count: currentCount,
-              onTap: () {
-                addOnZikr();
-              },
-            ),
-          ],
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 24),
+        child: Visibility(
+          visible: !isCycleCompleted,
+          replacement: AzkarCompletedView(
+            onNext: () {
+              setState(() {
+                currentZikrCount = 0;
+                currentZikrIndex = 0;
+                isCycleCompleted = false;
+              });
+            },
+          ),
+          child: Column(
+            spacing: 16,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                azkarList[currentZikrIndex].aya,
+                style: AppStyles.bold20White.copyWith(fontSize: 36),
+                textAlign: TextAlign.center,
+              ),
+              Sebha(
+                currentZikr: azkarList[currentZikrIndex].name,
+                count: currentZikrCount,
+                onTap: () {
+                  plusZikr();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void addOnZikr() {
+  void plusZikr() {
     setState(() {
-      if (currentCount == 33) {
-        currentCount = 0;
+      if (currentZikrCount > 31) {
+        if (currentZikrIndex == azkarList.length - 1) isCycleCompleted = true;
+        currentZikrCount = 0;
         currentZikrIndex = (currentZikrIndex + 1) % azkarList.length;
       } else {
-        currentCount++;
+        currentZikrCount++;
       }
     });
   }
