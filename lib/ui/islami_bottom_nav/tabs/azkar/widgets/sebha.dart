264dfdb6d849c532e9254a90d0795f46697c8d14
DELL <dev.mahmoudtarek@gmail.com>
2026-03-05 23:26:43 +0200

    Adjust `InkWell` overlay color and disable the default splash factory.

--- lib/ui/islami_bottom_nav/tabs/azkar/widgets/sebha.dart
+++ lib/ui/islami_bottom_nav/tabs/azkar/widgets/sebha.dart
@@ -4,10 +4,7 @@ import 'package:islami/utils/app_colors.dart';
 import 'package:islami/utils/app_styles.dart';
 
 class Sebha extends StatelessWidget {
-  static const double _radius = 220;
-  static const double _topPadding = 76;
   static const double _contentTopPadding = 26;
-
   final String currentZikr;
   final int count;
   final VoidCallback? onTap;
@@ -23,55 +20,52 @@ class Sebha extends StatelessWidget {
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
     final isPortrait = media.orientation == Orientation.portrait;
-
-    final height = isPortrait
-        ? media.size.height * .4
-        : media.size.height * .8;
-
-    final width = isPortrait
-        ? media.size.width * .9
-        : media.size.width * .4;
+    final height = isPortrait ? media.size.height * .4 : media.size.height * .8;
+    final width = isPortrait ? media.size.width * .9 : media.size.width * .4;
 
     return Stack(
       alignment: Alignment.topCenter,
       children: [
         Image.asset(AppImages.bgAzkarCounterTag),
-
         Padding(
-          padding: const EdgeInsets.only(top: _topPadding),
+          padding: const EdgeInsets.only(top: 76),
           child: Material(
+            shape: const CircleBorder(),
+            clipBehavior: Clip.antiAlias,
+            animateColor: true,
+            elevation: 20,
+            type: MaterialType.button,
             color: Colors.transparent,
-            borderRadius: BorderRadius.circular(_radius),
             child: InkWell(
-              onTap: onTap,
-              borderRadius: BorderRadius.circular(_radius),
               overlayColor: WidgetStatePropertyAll(
-                AppColors.darkGray.withValues(alpha: 0.5),
+                AppColors.darkGray.withValues(alpha: .6),
               ),
-              child: Ink.image(
-                height: height,
+              onTap: onTap,
+              splashFactory: NoSplash.splashFactory,
+              child: Ink(
                 width: width,
-                image: const AssetImage(AppImages.bgAzkarCounter),
-                fit: BoxFit.fill,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: _contentTopPadding),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        currentZikr,
-                        textAlign: TextAlign.center,
-                        style: AppStyles.bold20White
-                            .copyWith(fontSize: 36),
-                      ),
-                      Text(
-                        '$count',
-                        style: AppStyles.bold20White
-                            .copyWith(fontSize: 36),
-                      ),
-                    ],
+                height: height,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(AppImages.bgAzkarCounter),
+                    fit: BoxFit.contain,
                   ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      currentZikr,
+                      textAlign: TextAlign.center,
+                      style: AppStyles.bold20White.copyWith(fontSize: 36),
+                    ),
+                    Text(
+                      '$count',
+                      style: AppStyles.bold20White.copyWith(fontSize: 36),
+                    ),
+                  ],
                 ),
               ),
             ),
