75df762ff338e4e02590c5e9156e703599caded5
DELL <dev.mahmoudtarek@gmail.com>
2026-03-13 20:45:51 +0200

    feat: Implement surah search functionality in QuranTab

--- lib/ui/islami_bottom_nav/tabs/quran/quran_tab.dart
+++ lib/ui/islami_bottom_nav/tabs/quran/quran_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami/models/surah.dart';
 import 'package:islami/ui/islami_bottom_nav/tabs/quran/widgets/most_recent_section.dart';
 import 'package:islami/ui/islami_bottom_nav/tabs/quran/widgets/surah_search_field.dart';
 import 'package:islami/ui/islami_bottom_nav/tabs/quran/widgets/surahs_list_section.dart';
@@ -9,10 +10,43 @@ import 'package:islami/utils/app_strings.dart';
 import 'package:islami/utils/app_styles.dart';
 import 'package:islami/utils/widgets/islami_header.dart';
 
-class QuranTab extends StatelessWidget {
+import '../../../../data/quran_repository.dart';
+
+class QuranTab extends StatefulWidget {
   const QuranTab({super.key});
 
   static const EdgeInsets defaultPadding = EdgeInsets.symmetric(horizontal: 20);
+
+  @override
+  State<QuranTab> createState() => _QuranTabState();
+}
+
+class _QuranTabState extends State<QuranTab> {
+  String surahSearch = '';
+  late final List<Surah> quranSurahs;
+
+  @override
+  void initState() {
+    super.initState();
+    quranSurahs = QuranRepository.getAllSurahs;
+  }
+
+  List<Surah> get filteredSurahs {
+    final search = surahSearch.trim().toLowerCase();
+
+    if (search.isEmpty) return quranSurahs;
+
+    return quranSurahs.where((surah) {
+      return surah.arabicName.contains(surahSearch) ||
+          surah.englishName.toLowerCase().contains(search);
+    }).toList();
+  }
+
+  void onSearchSurahChanged(String value) {
+    setState(() {
+      surahSearch = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +57,21 @@ class QuranTab extends StatelessWidget {
       content: CustomScrollView(
         slivers: [
           paddedSliver(
-            padding: defaultPadding.copyWith(right: 48.w, left: 48.w),
+            padding: QuranTab.defaultPadding.copyWith(right: 48.w, left: 48.w),
             child: const IslamiHeader(),
           ),
-
           paddedSliver(
-            padding: defaultPadding.copyWith(bottom: 10.h),
-            child: const SurahSearchField(),
+            padding: QuranTab.defaultPadding.copyWith(bottom: 10.h),
+            child: SurahSearchField(onChangeSurahSearch: onSearchSurahChanged),
           ),
-
-          titleSliver(AppStrings.homeMostRecentlyTitle),
-
-          SliverToBoxAdapter(child: MostRecentSection()),
-
+          if (surahSearch.isEmpty) ...[
+            titleSliver(AppStrings.homeMostRecentlyTitle),
+            const SliverToBoxAdapter(child: MostRecentSection()),
+          ],
           titleSliver(AppStrings.homeSurahsListTitle),
-
           SliverPadding(
             padding: EdgeInsets.fromLTRB(20.w, 0.h, 20.w, 10.h),
-            sliver: SurahsSection(),
+            sliver: SurahsSection(quranSurahs: filteredSurahs),
           ),
         ],
       ),
@@ -58,7 +89,7 @@ class QuranTab extends StatelessWidget {
 
   SliverToBoxAdapter titleSliver(String title) {
     return paddedSliver(
-      padding: defaultPadding.copyWith(bottom: 10.h, top: 10.h),
+      padding: QuranTab.defaultPadding.copyWith(bottom: 10.h, top: 10.h),
       child: Text(title, style: AppStyles.base16BoldCreamyYellow),
     );
   }
